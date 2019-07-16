127213be0f55e782c4d444d91c39d488ba035137
Jakub Michniewicz <jmic@tapptic.com>
2019-07-16 12:28:22 +0200

    Refactor the code of scrapper.js

--- scraper.js
+++ scraper.js
@@ -1,5 +1,4 @@
 const puppeteer = require("puppeteer");
-var moment = require("moment");
 
 (async () => {
   const browser = await puppeteer.launch();
@@ -20,34 +19,16 @@ var moment = require("moment");
   const links = await page.evaluate(resultsSelector => {
     const nodesArray = Array.from(document.querySelectorAll(resultsSelector));
 
-    return nodesArray.map(node => {
-      const title =
-        node.firstElementChild.firstElementChild.firstElementChild
-          .firstElementChild.firstElementChild.alt;
-      const link =
-        node.firstElementChild.firstElementChild.firstElementChild
-          .firstElementChild.href;
-      const price =
-        node.firstElementChild.firstElementChild.children[2].firstElementChild
-          .children[0].innerText;
-      const localization =
-        node.firstElementChild.lastElementChild.firstElementChild
-          .firstElementChild.firstElementChild.children[0].innerText;
-      const date =
-        node.firstElementChild.lastElementChild.firstElementChild
-          .firstElementChild.firstElementChild.children[1].innerText;
-
-      return {
-        title: title,
-        link: link,
-        price: price,
-        localization: localization,
-        date: date
-      };
-    });
+    return nodesArray.map(node => ({
+      title: node.querySelector('h3 a strong').innerText,
+      link: node.querySelector('h3 a').href,
+      price: node.querySelector('p.price strong').innerText,
+      localization: node.querySelector('.bottom-cell small:first-child span').innerText,
+      date: node.querySelector('.bottom-cell small:last-child span').innerText
+    }));
   }, resultsSelector);
 
-  console.log(links);
+  console.log(links.length);
 
   await browser.close();
 })();
